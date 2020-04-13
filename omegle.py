9bcc930dd5662ce677b45034345e567d31131a7b
William brochensque junior <brochensquewill@protonmail.com>
2020-04-12 23:24:30 -0300

    init

---
+++ omegle.py
@@ -0,0 +1,63 @@
+# coding: utf-8
+
+from selenium import webdriver
+import time
+
+textToSend = """
+hey i'm a bot, beep bop
+"""
+
+browser = webdriver.Firefox(executable_path='geckodriver')
+
+browser.get('https://www.omegle.com')
+browser.maximize_window()
+time.sleep(3)
+
+ChatButton = browser.find_element_by_id('textbtn').click()
+time.sleep(2)
+
+alreadySent = False
+LastAnswer = False
+
+while True:
+	try:
+		content = browser.page_source
+		Disconnected = content.find('Stranger has disconnected')
+		IsAnswered = content.find('Stranger:')
+
+		if Disconnected != -1:
+			DisconnectButton = browser.find_element_by_class_name("disconnectbtn").click()
+			time.sleep(2)
+			alreadySent = False
+			print("Conversation Ended")
+
+		else:
+
+			if alreadySent == False:
+				TextBox = browser.find_element_by_class_name("chatmsg")
+				TextBox.send_keys(textToSend)
+				time.sleep(1)
+				SendButton = browser.find_element_by_class_name("sendbtn").click()
+				alreadySent = True
+
+			else:
+						
+				if IsAnswered != -1:
+
+					query = browser.find_elements_by_class_name("strangermsg")
+					answer = query[-1].find_element_by_tag_name("span")
+
+					if LastAnswer != answer.text:
+
+						print(answer.text)
+						LastAnswer = answer.text
+
+					else:
+						pass
+
+					time.sleep(2)
+
+				else:
+					pass
+	except:
+		pass
